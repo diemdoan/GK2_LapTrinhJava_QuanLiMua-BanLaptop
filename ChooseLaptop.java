b1d4dddb55b0c972613250423a46a060c8da85da
diemdoan <diemnek7890@gmail.com>
2024-04-09 22:05:34 +0700

    diem toi ngay4/9

--- ChooseLaptop.java
+++ ChooseLaptop.java
@@ -38,20 +38,6 @@ public class ChooseLaptop {
 		frame.setResizable(false);
 		frame.setVisible(true);
 
-		JButton btnNewButton = new JButton("Thanh toán");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 27));
-		btnNewButton.setBounds(1067, 617, 181, 39);
-		frame.getContentPane().add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				new Customer();
-				frame.dispose();
-			}
-		});
-
 		JButton btnNewButton_1 = new JButton("Đăng xuất");
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 23));
 		btnNewButton_1.setBounds(39, 27, 161, 37);
@@ -174,36 +160,42 @@ public class ChooseLaptop {
 			public void actionPerformed(ActionEvent e) {
 				int selectedRow = table.getSelectedRow();
 				if (selectedRow != -1) {
-					String tenLaptop = table.getValueAt(selectedRow, 1).toString();
-					double giaLaptop = Double.parseDouble(table.getValueAt(selectedRow, 6).toString());
-
-					int selectedRow_tt = -1;
-					for (int i = 0; i < model_tt.getRowCount(); i++) {
-						if (model_tt.getValueAt(i, 0).toString().equals(tenLaptop)) {
-							selectedRow_tt = i;
-							break;
-						}
-					}
-					if (selectedRow_tt == -1) {						
-						model_tt.addRow(new Object[] { tenLaptop, 1, giaLaptop }); 
-					} else {
-						int soLuong = Integer.valueOf(model_tt.getValueAt(selectedRow_tt, 1).toString());
-						double gia = Double.valueOf(model_tt.getValueAt(selectedRow_tt, 2).toString());
-						model_tt.setValueAt(soLuong + 1, selectedRow_tt, 1); 
-						model_tt.setValueAt(gia + giaLaptop, selectedRow_tt, 2); 
-					}
-
-					int soLuongLaptop = (int) table.getValueAt(selectedRow, 5);
-					table.setValueAt(soLuongLaptop - 1, selectedRow, 5);
-					int soLuong = Integer.parseInt(model.getValueAt(selectedRow, 5).toString()) - 1;
-					model.setValueAt(soLuong, selectedRow, 5);
-
-					String sqlUpdate = "UPDATE laptop SET soluong = ?";
+					String selectedID = (String) table.getValueAt(selectedRow, 0);
 					try {
 						Connection conn = ConnectSQL.getConnection();
-						PreparedStatement preparedStatement = conn.prepareStatement(sqlUpdate);
-						preparedStatement.setInt(1, soLuong);
-						preparedStatement.executeUpdate();
+						Statement statement = conn.createStatement();
+						String selectQuery = "SELECT ten, gia, soluong FROM laptop WHERE ID_laptop = " + selectedID;
+						ResultSet resultSet = statement.executeQuery(selectQuery);
+						if (resultSet.next()) {
+							String tenLaptop = resultSet.getString("ten");
+							double giaLaptop = resultSet.getDouble("gia");
+
+							boolean isExist = false;
+							int currentRow = -1;
+							for (int i = 0; i < model_tt.getRowCount(); i++) {
+								String id = (String) model_tt.getValueAt(i, 0);
+								if (id.equals(selectedID)) {
+									isExist = true;
+									currentRow = i;
+									break;
+								}
+							}
+
+							if (isExist) {
+								int currentSoLuong = (int) model_tt.getValueAt(currentRow, 2);
+								double currentGia = (double) model_tt.getValueAt(currentRow, 3);
+								model_tt.setValueAt(currentSoLuong + 1, currentRow, 2);
+								model_tt.setValueAt(currentGia + giaLaptop, currentRow, 3);
+							} else {
+								model_tt.addRow(new Object[] { selectedID, tenLaptop, 1, giaLaptop });
+							}
+
+							String updateQuery = "UPDATE laptop SET SoLuong = SoLuong - 1 WHERE ID_laptop = "
+									+ selectedID;
+							statement.executeUpdate(updateQuery);
+
+						}
+						resultSet.close();
 					} catch (SQLException ex) {
 						ex.printStackTrace();
 					}
@@ -219,30 +211,111 @@ public class ChooseLaptop {
 		scrollPane_tt.setViewportView(table_tt);
 
 		model_tt = new DefaultTableModel();
+		model_tt.addColumn("ID");
 		model_tt.addColumn("Tên Laptop");
 		model_tt.addColumn("Số lượng");
 		model_tt.addColumn("Giá");
 		table_tt.setModel(model_tt);
 
+		JButton btnNewButton = new JButton("Thanh toán");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton.setBounds(1092, 498, 134, 33);
+		frame.getContentPane().add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double totalPrice = 0;
+				for (int i = 0; i < model_tt.getRowCount(); i++) {
+					double gia = (double) model_tt.getValueAt(i, 3);
+					int soLuong = (int) model_tt.getValueAt(i, 2);
+					totalPrice += gia * soLuong;
+				}
+				textField_tong.setText(String.valueOf(totalPrice));
+			}
+		});
+
 		textField_tong = new JTextField();
 		textField_tong.setColumns(10);
-		textField_tong.setBounds(1080, 572, 158, 27);
+		textField_tong.setBounds(1077, 567, 171, 27);
 		frame.getContentPane().add(textField_tong);
 
 		JLabel lblNewLabel_1_1 = new JLabel("Tổng tiền");
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 21));
-		lblNewLabel_1_1.setBounds(1102, 526, 111, 39);
+		lblNewLabel_1_1.setBounds(1109, 529, 111, 39);
 		frame.getContentPane().add(lblNewLabel_1_1);
+
+		JButton btninThngTin = new JButton("Điền thông tin ");
+		btninThngTin.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btninThngTin.setBounds(1080, 628, 168, 50);
+		frame.getContentPane().add(btninThngTin);
+		btninThngTin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Customer();
+				frame.dispose();
+			}
+		});
 
 		JButton btn_rfr = new JButton("Refresh");
 		btn_rfr.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btn_rfr.setBounds(169, 628, 93, 27);
 		frame.getContentPane().add(btn_rfr);
-
 		btn_rfr.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				searchLaptop();
 			}
+		});
+
+		JButton btn_xoa = new JButton("Xóa");
+		btn_xoa.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btn_xoa.setBounds(574, 455, 84, 27);
+		frame.getContentPane().add(btn_xoa);
+
+		btn_xoa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int selectedRow = table_tt.getSelectedRow();
+				if (selectedRow >= 0) {
+					Object selectedId = table_tt.getValueAt(selectedRow, 0);
+
+					try {
+						Connection connection = ConnectSQL.getConnection();
+						String selectQuery = "SELECT soluong FROM laptop WHERE ID_laptop = " + selectedId;
+						PreparedStatement selectStatement = connection.prepareStatement(selectQuery);
+						ResultSet resultSet = selectStatement.executeQuery();
+
+						if (resultSet.next()) {
+							Object soluongFromDB = resultSet.getObject("soluong");
+							Object soluongFromTable = table_tt.getValueAt(selectedRow, 2);
+
+							int slFromDB = (soluongFromDB != null) ? (int) soluongFromDB : 0;
+							int slFromTable = (soluongFromTable != null) ? (int) soluongFromTable : 0;
+
+							int sl = slFromDB + slFromTable;
+							String sql = "UPDATE laptop SET soluong = ? WHERE ID_laptop = ?";
+							PreparedStatement statement = connection.prepareStatement(sql);
+							statement.setInt(1, sl);
+							statement.setObject(2, selectedId);
+							statement.executeUpdate();
+							statement.close();
+						}
+
+						resultSet.close();
+						selectStatement.close();
+
+						connection.close();
+
+						searchLaptop();
+					} catch (SQLException ex) {
+						ex.printStackTrace();
+					}
+
+					DefaultTableModel model = (DefaultTableModel) table_tt.getModel();
+					model.removeRow(selectedRow);
+
+				} else {
+					JOptionPane.showMessageDialog(frame, "Vui lòng chọn một hàng để xóa.", "Lỗi",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+
 		});
 	}
 
@@ -296,7 +369,7 @@ public class ChooseLaptop {
 				String soluong = resultSet.getString("soluong");
 				String gia = resultSet.getString("gia");
 
-				model.addRow(new Object[] { ID, ten, nam, hang, ch, soluong, gia + " VND"});
+				model.addRow(new Object[] { ID, ten, nam, hang, ch, soluong, gia + " VND" });
 			}
 
 			statement.close();
